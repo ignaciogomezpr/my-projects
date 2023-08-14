6e70a5e7c5a5ffa8fafdb56ec45fc1ebe0d2fa48
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-14 19:34:54 +0000

    Mon, Aug 14, 2023, 3:34 PM -04:00

--- population/population.c
+++ population/population.c
@@ -4,28 +4,27 @@
 int main(void)
 {
     // TODO: Prompt for start size
-int population;
-do
-{
- population = get_int("Whats the llama population size?\n");
-}
-while (population < 9);
+    int population;
+    do
+    {
+        population = get_int("Whats the llama population size?\n");
+    }
+    while (population < 9);
     // TODO: Prompt for end size
-int endsize;
-do
-{
-    endsize = get_int("Whats the llama population's end size?\n");
-}
-     while (endsize < population);
+    int endsize;
+    do
+    {
+        endsize = get_int("Whats the llama population's end size?\n");
+    }
+    while (endsize < population);
     // TODO: Calculate number of years until we reach threshold
-int years = 0;
-while (population < endsize)
-{
-    population = population + population/3 - population/4;
-    years = years + 1;
-}
+    int years = 0;
+    while (population < endsize)
+    {
+        population = population + population / 3 - population / 4;
+        years = years + 1;
+    }
 
     // TODO: Print number of years
-     printf ("Years: %i \n", years);
+    printf("Years: %i \n", years);
 }
-
