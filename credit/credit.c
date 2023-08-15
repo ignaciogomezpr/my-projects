7cd8f2a840ea8caa5b4304af3027f6ef6e36027a
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-15 17:49:48 +0000

    Tue, Aug 15, 2023, 1:49 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,12 +7,12 @@ int main(void)
     int n = get_long("Number: ");
 // Verify if it is valid
     // Multiply every other number by 2
-    for (long i = 1; i < n; i ** 10)
+    for (long i = 1; i < n; i * 10)
     {
-        long r = n % 0
-        long sum =+ r
+        long r = n % 0;
+        long sum =+ r;
     }
-    printf("%li ", sum)
+    printf("%li ", sum);
     // Add Products
     // Add Everything
     // Last digit in sum == 0?
