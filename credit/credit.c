2fdbff36428182f6f1252c1ea7ef03baf8203e9b
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-15 17:33:14 +0000

    Tue, Aug 15, 2023, 1:33 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,5 +4,11 @@
 int main(void)
 {
 //Get Credit Card #
+    int n = get_long("Number: ");
+//Verify if it is valid
+
+//Verify if it is Master Card
+//Verify if it is Amex
+//Verify if it is Visa
 
 }
