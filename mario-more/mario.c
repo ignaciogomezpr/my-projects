2b56aaf831b4331d1a85992b4850f3d84766b3f0
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-15 15:53:11 +0000

    Tue, Aug 15, 2023, 11:53 AM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+//Get pyramid size
+    int size;
+    do
+    {
+    size = get_int("What's the pyramid size? ");
+    }
+    while (1 >= size >= 8);
+//Creates pyramid
+
+    for (int i = 0; i = size; i++)
+    {
+        while 
+    }
 }
