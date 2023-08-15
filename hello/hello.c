3eb603037a5f4bbd187f8b5d1e94c6c53f321c6a
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-15 15:19:45 +0000

    Tue, Aug 15, 2023, 11:19 AM -04:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void);
+int main(void)
 
+{
 // Asks name
-{
-    string name = get_string("What is your name?" );
-}
+    string name = get_string("What is your name?  ");
+
 // Print "Hello, Name"
-{
+
     printf("Hello, %s\n", name);
 }
