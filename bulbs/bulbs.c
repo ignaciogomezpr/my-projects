a42ea7fa70406490d59259884464a7a53e063241
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 15:11:01 +0000

    Mon, Aug 28, 2023, 11:11 AM -04:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -8,14 +8,14 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    int decimals[];
+
     string msg = get_string("What's your bulb message? " );
+    
         for (int i = 0, lngt = strlen(msg); i < lngt; i++)
         {
-            decimals[i] = int msg[i]
+            decimals[i] = int msg[i];
         }
-
-
+    printf("Your decimal array: %i", msg);
 }
 
 void print_bulb(int bit)
