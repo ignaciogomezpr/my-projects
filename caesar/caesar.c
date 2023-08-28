3b1a53f693693b17790ad3ba8d58d0f687188d1b
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 19:04:06 +0000

    Mon, Aug 28, 2023, 3:04 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,11 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     if (argc < 3 && isalpha(argv[1]) = 0 && isalnum(argv[1]) != 0)
     {
-        printf ("working")
+        printf ("working");
     }
 
 }
