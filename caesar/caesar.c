b69bcd0112a3c555cefd72f6f8c1fdf86e20ac6c
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 19:23:26 +0000

    Mon, Aug 28, 2023, 3:23 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,22 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    int numbers[] = {0,1,2,3,4,5,6,7,8,9}
+    int numbers[] = {0,1,2,3,4,5,6,7,8,9};
     if (argc < 3)
     {
-        for (int i = 1, lngt = strlen(argv[]); i < (lngt + 1) ; i++)
+        for (int i = 1, lngt = strlen(argv); i < (lngt + 1) ; i++)
         {
             for (c = 0; c < 9, c++)
             {
                 if argv[i] != numbers[c]
                 {
-                printf ("Usage: ./caesar key" )
+                printf ("Usage: ./caesar key" );
                 }
             }
         }
     }
-
 }
