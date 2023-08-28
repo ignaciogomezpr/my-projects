261e0d888f75e688d7b85aa7c5ba935cf715c581
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 19:31:20 +0000

    Mon, Aug 28, 2023, 3:31 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,17 +6,26 @@
 int main(int argc, string argv[])
 {
     int numbers[] = {0,1,2,3,4,5,6,7,8,9};
+
+    //Check if user inputs one key
     if (argc != 2)
     {
+    printf ("Usage: ./caesar key" );
+    return 1;
+    }
+
+
+
+
+
+
+
+
         for (int i = 1, lngt = strlen(argv[1]), i < (lngt + 1) ; i++)
         {
             for (int c = 0; c < 9, c++)
             {
                 if argv[1][i] != numbers[c]
                 {
-                printf ("Usage: ./caesar key" );
+
                 }
-            }
-        }
-    }
-}
