80ce13c6f684d7da04b51970334e9c3f6584dd61
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 19:33:39 +0000

    Mon, Aug 28, 2023, 3:33 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,8 +7,8 @@ int main(int argc, string argv[])
 {
     int numbers[] = {0,1,2,3,4,5,6,7,8,9};
 
-    //Check if user inputs one key
-    if (argc != 2)
+    // Check if user inputs one key
+    if ( 1 < argc > 2)
     {
     printf ("Usage: ./caesar key" );
     return 1;
