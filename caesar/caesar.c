c4b7e1d8a1dce7f83a6f5d88c0ecdeea5124d8b0
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-08-28 19:16:56 +0000

    Mon, Aug 28, 2023, 3:16 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,11 +4,15 @@
 
 int main(int argc, string argv[])
 {
+    int numbers[] = {0,1,2,3,4,5,6,7,8,9}
     if (argc < 3)
     {
-    for (int i = 0, lngt = strlen(word); i < lngt; i++)
+        for (int i = 0, lngt = strlen(argv[]); i < lngt; i++)
         {
-
+            for (c = 0; c < 9, c++)
+            {
+            if argv[i] != 
+            }
         }
     }
 
