ecc0306f057aa60f42852c18091598185d95bbdd
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-09-13 01:11:30 +0000

    Tue, Sep 12, 2023, 9:11 PM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -53,7 +53,7 @@ int main(int argc, string argv[])
         string name = get_string("Vote: ");
 
         // Check for invalid vote
-        if (!vote(name))
+        if (vote(name))
         {
             printf("Invalid vote.\n");
         }
