7fd6dded38f1b0b58efde80fee83668cfa0c37c9
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-09-13 01:00:15 +0000

    Tue, Sep 12, 2023, 9:00 PM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,9 +69,9 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (name = candidate[i].name)
+        if (name = candidates[i].name)
         {
-            candidate[i].votes++;
+            candidates[i].votes++;
             return true;
         }
     }
