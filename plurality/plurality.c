da626b9fa091e326640f6021293b31825d2b31a5
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-09-13 01:15:12 +0000

    Tue, Sep 12, 2023, 9:15 PM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -75,13 +75,15 @@ bool vote(string name)
             return true;
         }
     }
-    // TODO
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].votes
+    }
     return;
 }
