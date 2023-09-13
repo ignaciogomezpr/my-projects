e1c004493327e9b67b3de71c9339467ef8755970
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-09-13 01:21:44 +0000

    Tue, Sep 12, 2023, 9:21 PM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,7 +83,7 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (candidates[i].votes > candidates )
         int most = candidates[i].votes;
 
     }
