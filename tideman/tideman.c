6a4bfa227535866c3fdb52fd422d85f9463d9e26
ignaciogomezpr <141792416+ignaciogomezpr@users.noreply.github.com>
2023-09-16 12:25:03 +0000

    Sat, Sep 16, 2023, 8:25 AM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -99,7 +99,13 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    if (name ==)
+    // TODO
+     for (int i = 0; i < MAX; i++)
+    {
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            ranks[] += ;
+        }
     return false;
 }
 
